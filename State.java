e76fbbc0715a0f1bc7c8e7c54cbcc43095116e29
Zerhouni Salim <74827906+Glycope@users.noreply.github.com>
2021-12-23 23:35:13 +0100

    Add files via upload

--- State.java
+++ State.java
@@ -17,6 +17,10 @@ public class State {
 		this.id = nbVisit++;
 	}
 	
+	public int getId() {
+		return id;
+	}
+	
 	public ArrayList<State> expand() {
 		ArrayList<State> succ = new ArrayList<State>();
 		for (City c : this.v.getNV()) {
@@ -35,12 +39,21 @@ public class State {
 		prim.add(v.getC());
 		double somme = 0;
 		while(!lcity.isEmpty()) {
+			double min;
 			int imin = 0;
-			double min = prim.get(0).distance(lcity.get(1));
+			boolean firstCity;
+			if (lcity.get(0).equals(City.listCity.get(0))) {
+				min = prim.get(0).distance(lcity.get(1));
+				firstCity = true;
+			}
+			else {
+				min = prim.get(0).distance(lcity.get(0));
+				firstCity = false;
+			}
 			for (int i = 0; i < prim.size(); i++) {
 				for (int j = 0; j < lcity.size(); j++) {
 					double d = prim.get(i).distance(lcity.get(j));
-					if (d < min && (i != 0 || j != 0)) {
+					if (d < min && !(i == 0 && j == 0 && firstCity)) {
 						min = d;
 						imin = j;
 					}
@@ -52,6 +65,40 @@ public class State {
 		}
 		return somme;
 	}
-
+	
+	public double evaluate() {
+		return this.h + this.costAction;
+	}
+	
+	public static ArrayList<State> AStar() {
+		ArrayList<State> explored = new ArrayList<State>();
+		ArrayList<State> succ = new ArrayList<State>();
+		explored.add(new State(new Visit(City.listCity.get(0),City.listCity), null, 0));
+		
+		int i = 0;
+		while (!explored.get(explored.size() - 1).v.isSolved()) {
+			if (i >= explored.size())
+				throw new ArrayIndexOutOfBoundsException();
+			succ.addAll(explored.get(i).expand());
+			State minState = explored.get(i);
+			double minf = minState.evaluate();
+			for (int j = 0; j < succ.size(); j++) {
+				double f = succ.get(j).evaluate();
+				if (f < minf) {
+					minState = explored.get(j);
+					minf = f;
+				}
+			}
+			explored.add(minState);
+			succ.remove(minState);
+		}
+		
+		ArrayList<State> solution = new ArrayList<State>();
+		solution.add(explored.get(explored.size() - 1));
+		while (!solution.get(0).equals(explored.get(0))) {
+			solution.add(0,solution.get(0).parent);
+		}
+		return solution;
+	}
 	
 }
