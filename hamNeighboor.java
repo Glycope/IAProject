48e919bbda8e25c0a9f1923ab253a2819bb977d8
Berkho <74465553+Berkho@users.noreply.github.com>
2021-12-29 21:33:17 +0100

    Update hamNeighboor.java

--- hamNeighboor.java
+++ hamNeighboor.java
@@ -5,19 +5,36 @@ public class hamNeighboor {
 
 	private Hamilton state;
 	private ArrayList<Hamilton> lVoisinage = new ArrayList<Hamilton>();
-	private static ArrayList<City> list; 
+	private static ArrayList<City> list;
+
+
 	public hamNeighboor(Hamilton s) {
 		this.state = s;
 		for(int i = 0; i < s.getCycle().size(); i++) {
 			for(int j = i+1; j < s.getCycle().size(); j++) {
-				list = new ArrayList<City>(s.getCycle());   //A optimiser niveau memoire
+				list = new ArrayList<City>(s.getCycle());   
 				Collections.swap(list, i, j);	
 				Hamilton ham = new Hamilton(s.getfCity());
-				ham.setCycle(list);
 				this.lVoisinage.add(ham);
 				list = null;
+				//this.TwoSwap(i, j);
 			}
 		}
+	}
+	
+	//fonction useless
+	public void TwoSwap(int i, int j) {
+		Hamilton ham = new Hamilton(this.state.getfCity());
+		list = new ArrayList<City>();
+		list.addAll(this.state.getCycle().subList(0, i));
+		for(int k = i; k <= j; k++) {
+			list.add(this.state.getCycle().get(j-(k-i)));
+		}
+		list.addAll(this.state.getCycle().subList(j+1, this.state.getCycle().size()));
+		ham.setCycle(list);
+		//System.out.println(list);
+		this.lVoisinage.add(ham);
+		list = null;
 	}
 	
 	public Hamilton getState() {
@@ -31,11 +48,17 @@ public class hamNeighboor {
 	public Hamilton hillClim() {
 		hamNeighboor current = this;
 		hamNeighboor bestHam = this;
+		Hamilton best = this.state;
+		Boolean found = false;
 		for(int i = 0; i < this.lVoisinage.size(); i++) {
-			if(this.lVoisinage.get(i).weight() <= bestHam.state.weight()) {
-				bestHam = null;
-				bestHam = new hamNeighboor(this.lVoisinage.get(i));
+			if(this.lVoisinage.get(i).getWeight() <= best.getWeight()) {
+				found = true;
+				best = this.lVoisinage.get(i);	
 			}
+		}
+		
+		if(found) {
+			bestHam = new hamNeighboor(best);
 		}
 		if(bestHam == current) {
 			return bestHam.state;
