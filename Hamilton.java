f131ba6ac9340296605d6b55afd0567864d7a16f
Berkho <74465553+Berkho@users.noreply.github.com>
2021-12-29 21:32:45 +0100

    Update Hamilton.java

--- Hamilton.java
+++ Hamilton.java
@@ -8,34 +8,45 @@ public class Hamilton { //pour la partie 2
 	private City villeDepart;
 	private ArrayList<City> cycle = new ArrayList<City>();
 	private ArrayList<Hamilton> lVoisinage = new ArrayList<Hamilton>();
+	private double weight = 0;
 	
-	public Hamilton(City v) { //prend un point de départ
+	public Hamilton(City v) { //prend un point de d�part
 		this.villeDepart = v;
-		this.generate();
 	}
 	
 	public void generate() { //genere un cycle hamiltonien initial (random)
 		Random random = new Random();
+		ArrayList<City> villes = new ArrayList<City>(City.listCity);
 		int j = 0;
 		int s = City.listCity.size();
 		for(int i = 0; i < s-1; i++) {
-			j = random.nextInt(s);
-			while(cycle.contains(City.listCity.get(j))) {
-				j = random.nextInt(s);
-			}
-			cycle.add(City.listCity.get(j));
-		} // C'est l'ordre qui définit le cycle
+			int u = villes.size();
+			j = random.nextInt(u);
+			cycle.add(villes.get(j));
+			villes.remove(j);
+		} // C'est l'ordre qui d�finit le cycle
+		weight = weight();
 	}
+	
 	
 	public double weight() { //heuristique?
 		double poids = 0;
 		int s = this.cycle.size();
-		for(int i = 1; i < s-1; i++) {
+		for(int i = 0; i < s-1; i++) {
 			poids += this.cycle.get(i).distance(this.cycle.get(i+1));
 		}
 		poids += this.cycle.get(s-1).distance(this.villeDepart);
+		poids += this.cycle.get(0).distance(this.villeDepart);
 		
 		return poids;
+	}
+	
+	public double getWeight() {
+		return this.weight;
+	}
+	
+	public void setCity(City v) {
+		this.villeDepart = v;
 	}
 	
 	public ArrayList<City> getCycle(){
@@ -52,6 +63,7 @@ public class Hamilton { //pour la partie 2
 	
 	public void setCycle(ArrayList<City> l) {
 		this.cycle = l;
+		weight = weight();
 	}
 	
 	public Hamilton go() {
