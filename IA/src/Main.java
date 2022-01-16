0c8a3250b30d7a199ea506b5937183f09783e543
Izabell Iskandar <52929224+izisk@users.noreply.github.com>
2022-01-16 23:45:24 +0100

    modif main

--- IA/src/Main.java
+++ IA/src/Main.java
@@ -18,6 +18,9 @@ public class Main {
 		double mAstar = 0;
 		double mLocal = 0;
 		double mLocal2 = 0;
+		double mcoutAstar = 0;
+		double mcoutLocal = 0;
+		double mcoutHam = 0;
 		for(int i = 0; i < k; i++) {
 			Generation.randomGeneration(x,y,n);
 			
@@ -25,13 +28,14 @@ public class Main {
 			long startTime1 = System.nanoTime();
 			System.out.println("L'algorithme A* : ");
 	        ArrayList<State> astarStates = AStar.algo_AStar();
-	        // on affiche la solution si le nombre de testes est inférieur à 5
+	        // on affiche la solution si le nombre de tests est inférieur à 5
 	        if (k < 5){
 	        	System.out.println(astarStates);
 	        }
 			long endTime1 = System.nanoTime();
 	        long duration1 = (endTime1 - startTime1);
 	        mAstar += duration1/1000000;
+	        mcoutAstar += astarStates.get(astarStates.size() - 1).getCA();
 			
 	        //HILL CLIMBING
 			Hamilton test = new Hamilton(City.listCity.get(0));
@@ -39,7 +43,7 @@ public class Main {
 			long startTime2 = System.nanoTime();
 			System.out.println("L'algorithme HillClimbing : ");
 			Hamilton u = test.go();
-			// on affiche la solution si le nombre de testes est inférieur à 5
+			// on affiche la solution si le nombre de tests est inférieur à 5
 			if (k < 5){
 				System.out.println(u.getCycle());
 				System.out.println(u.getWeight());
@@ -52,13 +56,14 @@ public class Main {
 			mHam += rate;
 			mHam2 += duration2/1000000;
 			mHam3 += rate2;
+			mcoutHam += u.getWeight();
 			
 			//LOCAL BEAM 
 			long startTime3 = System.nanoTime();
 			System.out.println("L'algorithme LocalBeamSearch : ");
 	        ArrayList<State> beamStates = LocalBeamSearch.algo_LocaLBeamSearch(kbeam);
 	        long endTime3 = System.nanoTime();
-	        // on affiche la solution si le nombre de testes est inférieur à 5
+	        // on affiche la solution si le nombre de tests est inférieur à 5
 	        if (k < 5){
 	        	System.out.println(beamStates);
 	        }
@@ -66,7 +71,7 @@ public class Main {
 			
 	        mLocal += duration3/1000000;
 			mLocal2 += (beamStates.get(beamStates.size() - 1).getCA() - astarStates.get(astarStates.size() - 1).getCA())/beamStates.get(beamStates.size() - 1).getCA();
-			
+			mcoutLocal += beamStates.get(beamStates.size() - 1).getCA();
 			
 		}
 		mAstar = mAstar/k; //temps d'exécution moyen Astar
@@ -75,13 +80,20 @@ public class Main {
 		mHam3 = mHam3/k; //Taux d'améliration entre la solution de Astar et la solution de HillClim
 		mLocal = mLocal/k;//Temps d'exécution LocalBeamSearch
 		mLocal2 = mLocal2/k; //Taux d'améliration entre la solution de Astar et la solution de LocalBeamSearch
+		mcoutAstar = mcoutAstar/k; //Coût total moyen pour l'algorithme Astar
+		mcoutLocal = mcoutLocal/k; //Coût total moyen pour l'algorithme HillClim
+		mcoutHam = mcoutHam/k; //Coût total moyen pour l'algorithme LocalBeamSearch
 		
-		System.out.println(" Temps d'exécution moyen de A* : " + mAstar + " ms");
-		System.out.println(" Temps d'exécution moyen de HillClim : " + mHam2 + " ms");
-		System.out.println(" Temps d'exécution moyen de LocalBeamSearch : " + mLocal + " ms");
+		
+		System.out.println("Temps d'exécution moyen de A* : " + mAstar + " ms");
+		System.out.println("Temps d'exécution moyen de HillClim : " + mHam2 + " ms");
+		System.out.println("Temps d'exécution moyen de LocalBeamSearch : " + mLocal + " ms");
 		System.out.println("Taux d'améliration moyen entre la solution initiale et la solution finale de HillClim : " + mHam);
 		System.out.println("Taux d'améliration moyen entre la solution de Astar et la solution de HillClim : " + mHam3);
 		System.out.println("Taux d'écart moyen entre la solution de Astar et la solution de LocalBeamSearch : " + mLocal2);
+		System.out.println("Coût total moyen pour l'algorithme Astar : " + mcoutAstar);
+		System.out.println("Coût total moyen pour l'algorithme HillClim : " + mcoutHam);
+		System.out.println("Coût total moyen pour l'algorithme LocalBeamSearch : " + mcoutLocal);
 		
 		
 	}
